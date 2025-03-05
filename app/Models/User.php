107383d753ad330adfee5161c76477e1b0f5e3db
longphan2k4 <ptlong2k4@gmail.com>
2025-03-05 16:12:56 +0700

    fix(auth):User_Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -25,5 +28,25 @@ class User extends Authenticatable
     public function groups()
     {
         return $this->hasMany(Group::class, 'judge_id');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public static function register(array $arr)
+    {
+        return self::create([
+            'username' => $arr['username'],
+            'email'    => $arr['email'],
+            'password' => Hash::make($arr['password']),
+            'role'     => $arr['role'],
+        ]);
     }
 }
